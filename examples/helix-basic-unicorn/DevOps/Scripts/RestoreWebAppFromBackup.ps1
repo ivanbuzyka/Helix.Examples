2681f3f134de56fefe872c6cbab649e379404671
ivanbuzyka <ivanbuzyka@gmail.com>
2019-12-04 14:58:13 +0100

    script for restoring of the web app form backup

---
+++ examples/helix-basic-unicorn/DevOps/Scripts/RestoreWebAppFromBackup.ps1
@@ -0,0 +1,10 @@
+param(	[string] $ResourceGroupName = "rgname",	
+	[string] $WebAppName = "webappname",
+	[string] $StorageAccountSasUrl = "https://ibudevops1.blob.core.windows.net/webapps?st=2019-12-03T10%3A23%3A00Z&se=2020-12-05T10%3A23%3A00Z&sp=racwdl&sv=2018-03-28&sr=c&sig=pgfGlLQRvN0sJtEnQbcAbSKdKEQLOLZ9nSzmn0%2BzObc%3D",
+	[string] $BackupName = "backupname"
+    )
+	
+	Write-Host "Restoring $WebAppName from $StorageAccountSasUrl..."
+	Restore-AzureRmWebAppBackup -ResourceGroupName $ResourceGroupName -Name $WebAppName -StorageAccountUrl $StorageAccountSasUrl -BlobName $BackupName -Overwrite
+	
+	Write-Host "Backup restoring scheduled..."
